c979060be47dd0ed1fd6b633d0b7d1c7dc5f7d56
Andrei <andreicampigotto@icloud.com>
2026-01-26 14:37:41 -0500

    fix screen

--- app/src/main/java/com/andreicampigotto/githubusersapi/presentation/MainActivity.kt
+++ app/src/main/java/com/andreicampigotto/githubusersapi/presentation/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.andreicampigotto.githubusersapi.R
 import com.andreicampigotto.githubusersapi.databinding.ActivityMainBinding
+import com.andreicampigotto.githubusersapi.presentation.user.UserFragment
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
 
 //    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -15,7 +16,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
 //        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            // Evita criar fragments duplicados após uma recriação da Activity
+            val fragmentUser = UserFragment()
+
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmente_user, fragmentUser)
+                .commit()
+        }
 
     }
 
